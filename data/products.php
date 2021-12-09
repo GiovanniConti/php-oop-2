e119a3cc26b413ced904655b4de2febd11de78bf
GiovanniConti <giovanni.conti@live.it>
2021-12-09 17:43:41 +0100

    Added data (users and products)

---
+++ data/products.php
@@ -0,0 +1,46 @@
+<?php
+
+$products = [
+  [
+    "category" => "tech",
+    "name" => "iPhone 13 pro max",
+    "type" => "smartphone",
+    "brand" => "Apple",
+    "price" => "1289",
+  ],
+  [
+    "category" => "tech",
+    "name" => "MacBook Pro 14",
+    "type" => "laptop",
+    "brand" => "Apple",
+    "price" => "2349",
+  ],
+  [
+    "category" => "tech",
+    "name" => "Apple watch",
+    "type" => "smartwatch",
+    "brand" => "Apple",
+    "price" => "439",
+  ],
+  [
+    "category" => "apparel",
+    "name" => "Jordan 4 retro fire red",
+    "type" => "shoes",
+    "brand" => "Jordan",
+    "price" => "400",
+  ],
+  [
+    "category" => "apparel",
+    "name" => "Adidas Yeezy Boost 350",
+    "type" => "shoes",
+    "brand" => "Adidas",
+    "price" => "350",
+  ],
+  [
+    "category" => "apparel",
+    "name" => "Bape x Canada Goose x Concepts Crofton",
+    "type" => "Hoodie",
+    "brand" => "BAPE",
+    "price" => "1400",
+  ],
+];
---
+++ data/users.php
@@ -0,0 +1,32 @@
+<?php
+
+$users = [
+  [
+    "name" => "Paolo",
+    "lastname" => "Rossi",
+    "mail" => "paolorossi@gmail.com",
+    "address" => "Via random, 1, (PA)",
+    "accountType" => "Standard"
+  ],
+  [
+    "name" => "Maria",
+    "lastname" => "Bianchi",
+    "mail" => "mariabianchi@gmail.com",
+    "address" => "Via random, 2, (MI)",
+    "accountType" => "Premium"
+  ],
+  [
+    "name" => "Giuseppe",
+    "lastname" => "Verdi",
+    "mail" => "giuseppeverdi@gmail.com",
+    "address" => "Via random, 3, (BO)",
+    "accountType" => "Standard"
+  ],
+  [
+    "name" => "Francesca",
+    "lastname" => "Bassi",
+    "mail" => "francescabassi@gmail.com",
+    "address" => "Via random, 4, (RM)",
+    "accountType" => "Premium"
+  ]
+];
